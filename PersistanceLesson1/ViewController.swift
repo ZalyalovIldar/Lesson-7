1c8c64ac3b3ed644ef5a1b068384713fea822719
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-11-20 15:49:22 +0300

    Initial Commit

---
+++ PersistanceLesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersistanceLesson1
+//
+//  Created by Ильдар Залялов on 20.11.2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
