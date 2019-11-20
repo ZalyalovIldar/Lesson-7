855e85c55a27e972e69d5314264177c50ff8fecd
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-11-20 16:49:39 +0300

    lesson commit

---
+++ PersistanceLesson1/UserSettingsStruct.swift
@@ -0,0 +1,14 @@
+//
+//  UserSettingsStruct.swift
+//  PersistanceLesson1
+//
+//  Created by Ильдар Залялов on 20.11.2019.
+//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//
+
+import Foundation
+
+struct UserSettingsStruct: Codable {
+    
+    var isAuthorized: Bool
+}
--- PersistanceLesson1/ViewController.swift
+++ PersistanceLesson1/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+enum UserDefaultKeys {
+    static let userSetting = "userSetting"
+    static let userSettingStruct = "userSettingStruct"
+}
+
+class ViewController: UIViewController {
+    
+    var userDefaults = UserDefaults.standard
+    
+    let keyForBool = "boolKey"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let settings = UserSettings(authorized: true)
+        
+        let settingsData = try? NSKeyedArchiver.archivedData(withRootObject: [settings, settings], requiringSecureCoding: false)
+        
+        userDefaults.set(settingsData, forKey: UserDefaultKeys.userSetting)
+        
+        ///Struct
+        
+        let encoder = JSONEncoder()
+        let settingsStruct = UserSettingsStruct(isAuthorized: true)
+        let jsonData = try! encoder.encode(settingsStruct)
+        
+        userDefaults.set(jsonData, forKey: UserDefaultKeys.userSettingStruct)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let userSettingsData = userDefaults.object(forKey: UserDefaultKeys.userSetting) as? Data,
+            let userSettings = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(userSettingsData) as? [UserSettings] {
+            
+            print("Settings: \(userSettings)")
+        }
+        
+        if let userSettingsData = userDefaults.object(forKey: UserDefaultKeys.userSettingStruct) as? Data {
+              
+            let decoder = JSONDecoder()
+            
+            guard let model = try? decoder.decode(UserSettingsStruct.self, from: userSettingsData) else { return }
+            
+            print("SettingsStruct: \(model)")
+        }
+        
+    }
 
 }
 
